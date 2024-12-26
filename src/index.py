0abbff2fee44b521ae77282f9cedc23c60e2142d
Hoang Hy <hoanghy01122003@gmail.com>
2024-12-27 02:33:53 +0700

    feat: update predict

--- src/index.py
+++ src/index.py
@@ -11,13 +11,33 @@ from tensorflow.keras import losses
 
 
 topic_model = tf.saved_model.load("./models/topic_model")
+topic_infer = topic_model.signatures["custom_inference"]
+
+sentiment_model = tf.saved_model.load("./models/sentiment_model")
+sentiment_infer = sentiment_model.signatures["custom_inference"]
+
+TOPIC = ["LECTURER", "TRAINING_PROGRAM", "FACILITY", "OTHER"]
+
+SENTIMENT = ["NEGATIVE", "NEUTRAL", "POSITIVE"]
+
+
+def predict_topic(sentence):
+    input_data = tf.constant([sentence])
+    output = topic_infer(input_data)
+    output_tensor = next(iter(output.values()))
+    argmax = tf.math.argmax(output_tensor, axis=1).numpy()[0]
+    return TOPIC[argmax]
+
+
+def predict_sentiment(sentence):
+    input_data = tf.constant([sentence])
+    output = sentiment_infer(input_data)
+    output_tensor = next(iter(output.values()))
+    argmax = tf.math.argmax(output_tensor, axis=1).numpy()[0]
+    return SENTIMENT[argmax]
+
 
 if __name__ == "__main__":
-    print(list(topic_model.signatures.keys()))
-
-    infer = topic_model.signatures["custom_inference"]
-
-    # Example input data
-    input_data = tf.constant(["gv"])  # Example input for a model with input_shape=(32,)
-    output = infer(input_data)
-    print("Inference output:", output)
+    sentence = "tài liệu thiếu thốn"
+    print("\n\nTopic:", predict_topic(sentence))
+    print("Sentiment:", predict_sentiment(sentence))
